e0977d842adc8513d39af2b7aa4d6f0b90a74c4b
서해윤 <ggomulll3@gmail.com>
2025-08-16 19:29:16 +0900

    test: 수동 테스트 작성

---
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -0,0 +1,19 @@
+package sample.cafekiosk.unit;
+
+import org.junit.jupiter.api.Test;
+import sample.cafekiosk.unit.beverage.Americano;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CafeKioskTest {
+
+    @Test
+    void add() {
+        CafeKiosk cafekiosk = new CafeKiosk();
+        cafekiosk.add(new Americano());
+
+        System.out.println(">>> 담긴 음료 수: " + cafekiosk.getBeverages().size());
+        System.out.println(">>> 담긴 음료: " + cafekiosk.getBeverages().get(0).getName());
+    }
+
+}
