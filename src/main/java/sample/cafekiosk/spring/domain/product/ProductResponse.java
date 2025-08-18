9e8ffb604810e87c9b774097648effcb0b7e68c1
서해윤 <ggomulll3@gmail.com>
2025-08-18 15:23:06 +0900

    feat: responseDto 생성

---
+++ src/main/java/sample/cafekiosk/spring/domain/product/ProductResponse.java
@@ -0,0 +1,31 @@
+package sample.cafekiosk.spring.domain.product;
+
+import lombok.Getter;
+
+@Getter
+public class ProductResponse {
+
+    private Long id;
+    private String productNumber;
+    private ProductType productType;
+    private SellingStatus sellingType;
+    private String name;
+    private int price;
+
+    private ProductResponse(Long id, String productNumber, ProductType productType, SellingStatus sellingType, String name, int price) {
+        this.id = id;
+        this.productNumber = productNumber;
+        this.productType = productType;
+    }
+
+    public static ProductResponse of(Product product) {
+        return new ProductResponse(
+                product.getId(),
+                product.getProductNumber(),
+                product.getProductType(),
+                product.getSellingStatus(),
+                product.getName(),
+                product.getPrice()
+        );
+    }
+}
