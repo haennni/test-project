8c880a5d2e82a2f8cebf4b050491f4f189d3f18d
서해윤 <ggomulll3@gmail.com>
2025-08-18 14:39:29 +0900

    feat: Entity 설계

---
+++ src/main/java/sample/cafekiosk/spring/domain/product/Product.java
@@ -0,0 +1,30 @@
+package sample.cafekiosk.spring.domain.product;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import sample.cafekiosk.spring.domain.BaseEntity;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Product extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productNumber;
+
+    @Enumerated(EnumType.STRING)
+    private ProductType productType;
+
+    @Enumerated(EnumType.STRING)
+    private ProductSellingType sellingType;
+
+    private String name;
+
+    private int price;
+
+}
