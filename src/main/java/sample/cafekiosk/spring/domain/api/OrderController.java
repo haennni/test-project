082e62ca938ee715815a98e34a65f8b5f1cef4d3
서해윤 <ggomulll3@gmail.com>
2025-08-20 13:06:52 +0900

    feat: OrderController 수정

--- src/main/java/sample/cafekiosk/spring/domain/api/OrderController.java
+++ src/main/java/sample/cafekiosk/spring/domain/api/OrderController.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import sample.cafekiosk.spring.domain.product.OrderCreateRequest;
-import sample.cafekiosk.spring.domain.product.OrderService;
+import sample.cafekiosk.spring.domain.product.dto.OrderCreateRequest;
+import sample.cafekiosk.spring.domain.service.OrderService;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/v1/orders")
@@ -16,6 +18,8 @@ public class OrderController {
 
     @PostMapping("new")
     public void createOrder(OrderCreateRequest request) {
-        orderService.createOrder(request);
+        LocalDateTime time = LocalDateTime.now();
+        orderService.createOrder(request, time);
+
     }
 }
