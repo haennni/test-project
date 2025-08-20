f644dea7d85bbc73df4d5ecc9b30e9b9e6edbaef
서해윤 <ggomulll3@gmail.com>
2025-08-20 14:05:24 +0900

    test: 주문 리스트에 같은 종류의 음료를 받아 주문을 생성하는 테스트 작성

--- src/test/java/sample/cafekiosk/spring/domain/product/OrderServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/product/OrderServiceTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import sample.cafekiosk.spring.domain.order.OrderProductRepository;
+import sample.cafekiosk.spring.domain.order.OrderRepository;
 import sample.cafekiosk.spring.domain.product.dto.OrderCreateRequest;
 import sample.cafekiosk.spring.domain.product.dto.OrderResponse;
 import sample.cafekiosk.spring.domain.service.OrderService;
@@ -26,10 +28,18 @@ class OrderServiceTest {
     @Autowired
     ProductRepository productRepository;
 
-/*    @AfterEach
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Autowired
+    OrderProductRepository orderProductRepository;
+
+    @AfterEach
     void tearDown() {
-        productRepository.deleteAll();
-    }*/
+        orderProductRepository.deleteAllInBatch();
+        productRepository.deleteAllInBatch();
+        orderRepository.deleteAllInBatch();
+    }
 
     @DisplayName("주문번호 리스트를 받아 주문을 생성한다.")
     @Test
@@ -55,6 +65,33 @@ class OrderServiceTest {
                 .containsExactlyInAnyOrder(
                         Tuple.tuple("001", 1000),
                         Tuple.tuple("002", 3000)
+                );
+    }
+
+    @DisplayName("주문 리스트에 같은 종류의 음료를 받아 주문을 생성한다.")
+    @Test
+    void duplicateProductOrder(){
+        // given
+        createProduct("001", ProductType.HANDMADE, 1000);
+        createProduct("002", ProductType.HANDMADE, 3000);
+        createProduct("003", ProductType.HANDMADE, 4000);
+
+        /* 주문 생성 */
+        OrderCreateRequest request = new OrderCreateRequest(List.of("001", "001"));
+        LocalDateTime now = LocalDateTime.now();
+        // when
+
+        OrderResponse orderResponse = orderService.createOrder(request, now);
+        // then
+        assertThat(orderResponse.getId()).isNotNull();
+        assertThat(orderResponse)
+                .extracting("registeredDateTime", "totalPrice")
+                .contains(now, 2000);
+        assertThat(orderResponse.getProducts()).hasSize(2)
+                .extracting("productNumber", "price")
+                .containsExactlyInAnyOrder(
+                        Tuple.tuple("001", 1000),
+                        Tuple.tuple("001", 1000)
                 );
     }
 
