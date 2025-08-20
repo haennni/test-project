65d994734084b6703bd2b59e9d0aa892dbb82ef8
서해윤 <ggomulll3@gmail.com>
2025-08-20 14:04:47 +0900

    feat: 중복되는 Product시 중복제거가 안되도록  로직 설계

--- src/main/java/sample/cafekiosk/spring/domain/service/OrderService.java
+++ src/main/java/sample/cafekiosk/spring/domain/service/OrderService.java
@@ -11,6 +11,8 @@ import sample.cafekiosk.spring.domain.product.dto.OrderResponse;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -20,8 +22,14 @@ public class OrderService {
     public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
         List<String> productNumbers = request.getProductNumbers();
         List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
+        Map<String, Product> productMap = products.stream()
+                .collect(Collectors.toMap(Product::getProductNumber, p -> p));
 
-        Order order = Order.create(products,  registeredDateTime);
+        List<Product> duplicateProducts = productNumbers.stream()
+                .map(productMap::get)
+                .toList();
+
+        Order order = Order.create(duplicateProducts,  registeredDateTime);
         orderRepository.save(order);
 
         return OrderResponse.of(order);
