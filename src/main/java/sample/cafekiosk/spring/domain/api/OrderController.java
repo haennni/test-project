20cde446410a4081e28c698a56038cc34c2888ac
서해윤 <ggomulll3@gmail.com>
2025-08-20 17:56:29 +0900

    feat: API 추가

--- src/main/java/sample/cafekiosk/spring/domain/api/OrderController.java
+++ src/main/java/sample/cafekiosk/spring/domain/api/OrderController.java
@@ -3,9 +3,8 @@ package sample.cafekiosk.spring.domain.api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import sample.cafekiosk.spring.domain.product.dto.OrderCreateRequest;
+import sample.cafekiosk.spring.domain.product.dto.request.OrderCreateRequest;
 import sample.cafekiosk.spring.domain.product.dto.OrderResponse;
 import sample.cafekiosk.spring.domain.service.OrderService;
 
--- src/main/java/sample/cafekiosk/spring/domain/api/ProductController.java
+++ src/main/java/sample/cafekiosk/spring/domain/api/ProductController.java
@@ -2,8 +2,12 @@ package sample.cafekiosk.spring.domain.api;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductResponse;
+import sample.cafekiosk.spring.domain.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.domain.service.ProductService;
 
 import java.util.List;
@@ -17,5 +21,12 @@ public class ProductController {
     @GetMapping("/api/v1/products/selling")
     public List<ProductResponse> getSellingProducts() {
         return productService.getSellingProducts();
+    }
+
+    @PostMapping("/api/v1/products/new")
+    public void createProduct(@RequestBody ProductCreateRequest request) {
+        // productNumber 만들기
+        // DB에서 마지막 저장된 Product의 상품 번호를 읽어와서 + 1
+
     }
 }
