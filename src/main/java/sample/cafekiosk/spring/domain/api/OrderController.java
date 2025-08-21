47e2eb639fb1e70787b803c04b8168499f9a620f
서해윤 <ggomulll3@gmail.com>
2025-08-21 14:18:44 +0900

    test: 주문 관련 테스트 작성

--- src/main/java/sample/cafekiosk/spring/domain/api/OrderController.java
+++ src/main/java/sample/cafekiosk/spring/domain/api/OrderController.java
@@ -1,6 +1,8 @@
 package sample.cafekiosk.spring.domain.api;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +19,9 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping("/api/v1/orders/new")
-    public OrderResponse createOrder(@RequestBody OrderCreateRequest request) {
+    public ApiResponse<OrderResponse> createOrder(@Valid @RequestBody OrderCreateRequest request) {
         LocalDateTime time = LocalDateTime.now();
-        return orderService.createOrder(request, time);
+        return ApiResponse.of(HttpStatus.OK, "성공적으로 주문을 생성하였습니다.", orderService.createOrder(request, time));
 
     }
 }
--- src/main/java/sample/cafekiosk/spring/domain/product/dto/request/OrderCreateRequest.java
+++ src/main/java/sample/cafekiosk/spring/domain/product/dto/request/OrderCreateRequest.java
@@ -1,5 +1,7 @@
 package sample.cafekiosk.spring.domain.product.dto.request;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +10,7 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 public class OrderCreateRequest {
+    @NotEmpty(message = "상품 번호 리스트는 필수입니다.")
     private List<String> productNumbers;
 
     public OrderCreateRequest(List<String> productNumbers) {
