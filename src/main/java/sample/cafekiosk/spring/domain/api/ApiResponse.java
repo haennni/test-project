24effd38d1fa7460a9f11409261c697b92526d5e
서해윤 <ggomulll3@gmail.com>
2025-08-21 14:17:22 +0900

    refactor: 공통 응답 수정

--- src/main/java/sample/cafekiosk/spring/domain/api/ApiResponse.java
+++ src/main/java/sample/cafekiosk/spring/domain/api/ApiResponse.java
@@ -10,23 +10,21 @@ public class ApiResponse<T> {
     private String message;
     private T data;
 
-    public ApiResponse(HttpStatus status, String message, T data) {
-        this.code = status.value();
-        this.status = status;
-        this.message = message;
-        this.data = data;
-    }
-
     public ApiResponse(HttpStatus status, T data) {
         this.code = status.value();
         this.status = status;
+        this.data = data;
+    }
+
+    public ApiResponse(int code, HttpStatus status, String message, T data) {
+        this.code = code;
+        this.status = status;
         this.message = message;
         this.data = data;
     }
 
-
     public static <T> ApiResponse<T> of(HttpStatus httpStatus, String message, T data) {
-        return new ApiResponse<>(httpStatus, message, data);
+        return new ApiResponse<>(httpStatus.value(), httpStatus, message, data);
     }
 
     public static <T> ApiResponse<T> of(HttpStatus httpStatus, T data) {
